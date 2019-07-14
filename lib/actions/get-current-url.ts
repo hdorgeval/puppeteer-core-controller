c02868f398046d528e063b2efe268acf192dae2e
hdorgeval <henri.dorgeval@outlook.fr>
2019-07-14 19:18:13 +0200

    chore(build): implement build script

--- lib/actions/get-current-url.ts
+++ lib/actions/get-current-url.ts
@@ -1,11 +1,9 @@
 import * as puppeteer from 'puppeteer-core';
+declare const window: Window;
 
 export async function getCurrentUrl(page: puppeteer.Page | undefined): Promise<string> {
   if (page) {
-    const url = await page.evaluate(
-      // eslint-disable-next-line no-undef
-      (): Promise<string> => Promise.resolve((window as Window).location.href),
-    );
+    const url = await page.evaluate((): Promise<string> => Promise.resolve(window.location.href));
     return url;
   }
   throw new Error('Cannot get current url because a new page has not been created');
