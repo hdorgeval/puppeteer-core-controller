973ce327d5f1a6e05f9654c89f8ff96d3d7ca620
hdorgeval <henri.dorgeval@outlook.fr>
2020-01-07 19:19:10 +0100

    feat(selector): add helper method exists() to the Selector API

---
+++ lib/selector/tests/exists/exists.test.ts
@@ -0,0 +1,159 @@
+import * as SUT from '../../../controller';
+import { LaunchOptions } from '../../../controller';
+import * as path from 'path';
+
+describe('Puppeteer Controller - Selector API - exists', (): void => {
+  let pptc: SUT.PuppeteerController;
+  beforeEach((): void => {
+    jest.setTimeout(30000);
+    pptc = new SUT.PuppeteerController();
+  });
+  afterEach(
+    async (): Promise<void> => {
+      await pptc.close();
+    },
+  );
+
+  test('should return false on wrong selector', async (): Promise<void> => {
+    // Given
+    const launchOptions: LaunchOptions = {
+      headless: true,
+    };
+    const url = `file:${path.join(__dirname, 'exists.test.html')}`;
+    await pptc.initWith(launchOptions).navigateTo(url);
+
+    // When
+    const selector = pptc.selector('foo').withText('bar');
+    const result = await selector.exists();
+
+    // Then
+    expect(result).toBe(false);
+  });
+
+  test('should return true when selector is visible', async (): Promise<void> => {
+    // Given
+    const launchOptions: LaunchOptions = {
+      headless: true,
+    };
+    const url = `file:${path.join(__dirname, 'exists.test.html')}`;
+    await pptc.initWith(launchOptions).navigateTo(url);
+
+    // When
+    const selector = pptc
+      .selector('[role="row"]')
+      .find('p')
+      .withText('I am visible');
+
+    const result = await selector.exists();
+
+    // Then
+    expect(result).toBe(true);
+  });
+
+  test('should return true when selector is hidden', async (): Promise<void> => {
+    // Given
+    const launchOptions: LaunchOptions = {
+      headless: true,
+    };
+    const url = `file:${path.join(__dirname, 'exists.test.html')}`;
+    await pptc.initWith(launchOptions).navigateTo(url);
+
+    // When
+    const selector = pptc
+      .selector('[role="row"]')
+      .find('p')
+      .withText('I am hidden');
+
+    const result = await selector.exists();
+    const isVisible = await selector.isVisible();
+
+    // Then
+    expect(result).toBe(true);
+    expect(isVisible).toBe(false);
+  });
+  test('should return true when selector is transparent', async (): Promise<void> => {
+    // Given
+    const launchOptions: LaunchOptions = {
+      headless: true,
+    };
+    const url = `file:${path.join(__dirname, 'exists.test.html')}`;
+    await pptc.initWith(launchOptions).navigateTo(url);
+
+    // When
+    const selector = pptc
+      .selector('[role="row"]')
+      .find('p')
+      .withText('I am transparent');
+
+    const result = await selector.exists();
+
+    // Then
+    expect(result).toBe(true);
+  });
+
+  test('should return true when selector is out of screen', async (): Promise<void> => {
+    // Given
+    const launchOptions: LaunchOptions = {
+      headless: true,
+    };
+    const url = `file:${path.join(__dirname, 'exists.test.html')}`;
+    await pptc.initWith(launchOptions).navigateTo(url);
+
+    // When
+    const selector = pptc
+      .selector('[role="row"]')
+      .find('p')
+      .withText('I am out of screen');
+
+    const result = await selector.exists();
+
+    // Then
+    expect(result).toBe(true);
+  });
+  test('should return true when selector is first hidden', async (): Promise<void> => {
+    // Given
+    const launchOptions: LaunchOptions = {
+      headless: true,
+    };
+    const url = `file:${path.join(__dirname, 'exists.test.html')}`;
+    await pptc.initWith(launchOptions).navigateTo(url);
+
+    // When
+    const selector = pptc
+      .selector('[role="row"]')
+      .find('td')
+      .withText('hidden, then visible')
+      .find('p'); //only the <p> ... </p> element is hidden first
+
+    const initialExistsStatus = await selector.exists();
+    await pptc.wait(5000);
+    const finalExistsStatus = await selector.exists();
+
+    // Then
+    expect(initialExistsStatus).toBe(true);
+    expect(finalExistsStatus).toBe(true);
+  });
+
+  test('should return true, even when selector is created before page is instanciated', async (): Promise<
+    void
+  > => {
+    // Given
+    const launchOptions: LaunchOptions = {
+      headless: true,
+    };
+
+    const selector = pptc
+      .selector('[role="row"]')
+      .find('p')
+      .withText('I am visible');
+
+    const url = `file:${path.join(__dirname, 'exists.test.html')}`;
+    await pptc.initWith(launchOptions).navigateTo(url);
+
+    // When
+    const result = await selector.exists();
+
+    // Then
+    expect(result).toBe(true);
+  });
+});
