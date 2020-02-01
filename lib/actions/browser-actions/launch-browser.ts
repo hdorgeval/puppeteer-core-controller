07b08815ca4cfbfc9b6edeb2e403cfb66bc71eaa
hdorgeval <henri.dorgeval@outlook.fr>
2020-02-01 11:17:54 +0100

    fix(browser-actions): fix automatic browser restart

--- lib/actions/browser-actions/launch-browser.ts
+++ lib/actions/browser-actions/launch-browser.ts
@@ -35,7 +35,7 @@ async function tryLaunchBrowser(options: Partial<LaunchOptions>): Promise<puppet
       if (
         error.message &&
         typeof error.message === 'string' &&
-        (error.message as string).includes('Failed to launch chrome') &&
+        (error.message as string).includes('Failed to launch the browser') &&
         (error.message as string).includes('Inconsistency detected by ld.so')
       ) {
         // eslint-disable-next-line no-console
